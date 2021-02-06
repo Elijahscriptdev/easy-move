f01c2a25156dbc69016a594083bf81de1e0bd674
Elijahscriptdev <elijahobominuru@gmail.com>
2021-02-06 03:23:14 +0100

    add nav component and also added antd

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -1,12 +1,23 @@
-import React from 'react'
-import './Hero.css'
+import React from "react";
+import "./Hero.css";
 
 const Hero = () => {
-    return (
-        <div>
-            hero
-        </div>
-    )
-}
+  return (
+    <div className="hero">
+      <div className=''>
+        <h1>
+          Lets's get you <span>Moving</span>
+        </h1>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+          <br />
+          Consequuntur suscipit nihil quia, inventore molestiae
+          <br />
+          quo qui tempora, ratione mollitia, repellendus obcaecati.
+        </p>
+      </div>
+    </div>
+  );
+};
 
-export default Hero
+export default Hero;
--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -1,12 +1,35 @@
-import React from 'react'
-import './NavBar.css'
+import React from "react";
+import "./NavBar.css";
+import { DatePicker } from "antd";
+import "antd/dist/antd.css";
 
 const NavBar = () => {
-    return (
-        <div className="">
-            
+  return (
+    <div className='nav'>
+      <nav className='navbar'>
+        <div className='logo'>
+          <img
+            src='https://res.cloudinary.com/elijjaaahhhh/image/upload/v1576158338/Group_3_uqrvap.jpg'
+            alt='logo'
+          />
         </div>
-    )
-}
+        <div className='links'>
+          <ul className='nav-items'>
+            <li className='nav-item'>
+              <a className='nav-link ' href=''>
+                Help
+              </a>
+            </li>
+            <li className='nav-item'>
+              <a className='nav-link-btn' href=''>
+                Hire a Truck
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
+};
 
-export default NavBar
+export default NavBar;
